ae0b2689d62a0ade89761a54ec25a131719c54dd
z604007z-Kyle <z604007z@gmail.com>
2026-01-06 16:18:15 +0800

    Delete aroma-studio-website directory

--- aroma-studio-website/js/script.js
+++
@@ -1,9 +0,0 @@
-// 基本導航平滑滾動（如果需要）
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
