d2d90634e4e52ca940f160d5cc32ed217c0c7583
z604007z-Kyle <z604007z@gmail.com>
2026-01-06 16:18:49 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 基本導航平滑滾動（如果需要）
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
